4ad293d25df7a39c6917843205177d343e2018e7
Viet-Anh Nguyen <61254534+vakoyomi@users.noreply.github.com>
2021-11-24 11:22:05 +0700

    Initial Commit

---
+++ KoyoCovid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KoyoCovid
+//
+//  Created by Viet-Anh Nguyen on 11/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KoyoCovid/KoyoCovidApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoyoCovidApp.swift
+//  KoyoCovid
+//
+//  Created by Viet-Anh Nguyen on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct KoyoCovidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
